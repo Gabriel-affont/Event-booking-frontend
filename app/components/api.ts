2d1055ac0b0e05ad5478c0112a32c373577a030e
Gabriel-affont <162576104+Gabriel-affont@users.noreply.github.com>
2026-01-11 11:22:58 +0300

    Update API URL for production

--- app/components/api.ts
+++ app/components/api.ts
@@ -1,10 +1,12 @@
-export const API_URL = "https://localhost:7092/api";
 
-// Always return an object (never undefined)
+export const API_URL = process.env.NEXT_PUBLIC_API_URL 
+  ? `${process.env.NEXT_PUBLIC_API_URL}/api`
+  : "http://localhost:5000/api";
+
 export function getAuthHeader(): HeadersInit {
   const token = localStorage.getItem("token");
   if (!token) {
-    return {}; // empty headers if no token
+    return {}; 
   }
   return {
     Authorization: `Bearer ${token}`,
